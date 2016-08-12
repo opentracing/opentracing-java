4cffc0d22d71492abee05af607f098aa81bda271
Ben Sigelman <bhs@lightstep.com>
2016-08-12 14:21:40 -0500

    Add a MockTracer to aid in unittests

---
+++ opentracing-api/src/main/java/io/opentracing/io/opentracing/mocktracer/MockTracer.java
@@ -0,0 +1,160 @@
+package io.opentracing.io.opentracing.mocktracer;
+
+import io.opentracing.References;
+import io.opentracing.Span;
+import io.opentracing.SpanContext;
+import io.opentracing.Tracer;
+import io.opentracing.propagation.Format;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * MockTracer makes it easy to test the semantics of OpenTracing instrumentation.
+ *
+ * By using a MockTracer as an io.opentracing.Tracer implementation for unittests, a developer can assert that Span
+ * properties and relationships with other Spans are defined as expected by instrumentation code.
+ *
+ * The MockTracerTest has simple usage examples.
+ */
+public class MockTracer implements Tracer {
+    private List<MockSpan> finishedSpans = new ArrayList<>();
+    private final Propagator propagator;
+
+    public MockTracer() {
+        this(Propagator.PRINTER);
+    }
+
+    /**
+     * Create a new MockTracer that passes through any calls to inject() and/or extract().
+     *
+     * @param propagator
+     */
+    public MockTracer(Propagator propagator) {
+        this.propagator = propagator;
+    }
+
+    /**
+     * Clear the finishedSpans() queue.
+     *
+     * Note that this does *not* have any effect on Spans created by MockTracer that have not finish()ed yet; those
+     * will still be enqueued in finishedSpans() when they finish().
+     */
+    public synchronized void reset() {
+        this.finishedSpans.clear();
+    }
+
+    /**
+     * @return a copy of all finish()ed MockSpans started by this MockTracer (since construction or the last call to
+     * MockTracer.reset()).
+     *
+     * @see MockTracer#reset()
+     */
+    public synchronized List<MockSpan> finishedSpans() {
+        return new ArrayList<>(this.finishedSpans);
+    }
+
+    /**
+     * Propagator allows the developer to intercept and verify any calls to inject() and/or extract().
+     *
+     * By default, MockTracer uses Propagator.PRINTER which simply logs such calls to System.out.
+     *
+     * @see MockTracer#MockTracer(Propagator)
+     */
+    public interface Propagator {
+        <C> void inject(MockSpan.MockContext ctx, Format<C> format, C carrier);
+        <C> MockSpan.MockContext extract(Format<C> format, C carrier);
+
+        static Propagator PRINTER = new Propagator() {
+            @Override
+            public <C> void inject(MockSpan.MockContext ctx, Format<C> format, C carrier) {
+                System.out.println("inject(" + ctx + ", " + format + ", " + carrier + ")");
+            }
+
+            @Override
+            public <C> MockSpan.MockContext extract(Format<C> format, C carrier) {
+                System.out.println("extract(" + format + ", " + carrier + ")");
+                return null;
+            }
+        };
+    }
+
+    @Override
+    public SpanBuilder buildSpan(String operationName) {
+        return new SpanBuilder(operationName);
+    }
+
+    @Override
+    public <C> void inject(SpanContext spanContext, Format<C> format, C carrier) {
+        this.propagator.inject((MockSpan.MockContext)spanContext, format, carrier);
+    }
+
+    @Override
+    public <C> SpanContext extract(Format<C> format, C carrier) {
+        return this.propagator.extract(format, carrier);
+    }
+
+    synchronized void appendFinishedSpan(MockSpan mockSpan) {
+        this.finishedSpans.add(mockSpan);
+    }
+
+    class SpanBuilder implements Tracer.SpanBuilder {
+        private final String operationName;
+        private long startMicros;
+        private MockSpan.MockContext firstParent;
+        private Map<String, Object> initialTags = new HashMap<>();
+
+        SpanBuilder(String operationName) {
+            this.operationName = operationName;
+        }
+        @Override
+        public Tracer.SpanBuilder asChildOf(SpanContext parent) {
+            return addReference(References.CHILD_OF, parent);
+        }
+
+        @Override
+        public Tracer.SpanBuilder asChildOf(Span parent) {
+            return addReference(References.CHILD_OF, parent.context());
+        }
+
+        @Override
+        public Tracer.SpanBuilder addReference(String referenceType, SpanContext referencedContext) {
+            if (firstParent == null && (
+                    referenceType == References.CHILD_OF || referenceType == References.FOLLOWS_FROM)) {
+                this.firstParent = (MockSpan.MockContext)referencedContext;
+            }
+            return this;
+        }
+
+        @Override
+        public Tracer.SpanBuilder withTag(String key, String value) {
+            this.initialTags.put(key, value);
+            return this;
+        }
+
+        @Override
+        public Tracer.SpanBuilder withTag(String key, boolean value) {
+            this.initialTags.put(key, value);
+            return this;
+        }
+
+        @Override
+        public Tracer.SpanBuilder withTag(String key, Number value) {
+            this.initialTags.put(key, value);
+            return this;
+        }
+
+        @Override
+        public Tracer.SpanBuilder withStartTimestamp(long microseconds) {
+            this.startMicros = microseconds;
+            return this;
+        }
+
+        @Override
+        public Span start() {
+            return new MockSpan(MockTracer.this, this.operationName, this.startMicros, initialTags, this.firstParent);
+        }
+    }
+}
