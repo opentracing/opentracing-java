80ad0770336136ce46e8f8ea20bb81599ff21b37
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2017-03-29 19:55:26 +0200

    Simplify 'if' by reversing double-negative 'and' to positive 'or'.

--- opentracing-util/src/main/java/io/opentracing/util/GlobalTracer.java
+++ opentracing-util/src/main/java/io/opentracing/util/GlobalTracer.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2016-2017 The OpenTracing Authors
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -86,10 +86,11 @@ public final class GlobalTracer implements Tracer {
             LOGGER.log(Level.FINE, "Attempted to register the GlobalTracer as delegate of itself.");
             return; // no-op
         }
-        if (!(globalTracer instanceof NoopTracer) && !globalTracer.equals(tracer)) {
+        if (globalTracer instanceof NoopTracer || globalTracer.equals(tracer)) {
+            globalTracer = tracer;
+        } else {
             throw new IllegalStateException("There is already a current global Tracer registered.");
         }
-        globalTracer = tracer;
     }
 
     @Override
