9039254a69a74bfda2605d3011918936a11de593
Brian Hanafee <bhanafee@gmail.com>
2017-04-30 22:32:33 -0700

    Restore expanded imports

--- opentracing-mock/src/main/java/io/opentracing/mock/MockTracer.java
+++ opentracing-mock/src/main/java/io/opentracing/mock/MockTracer.java
@@ -13,7 +13,11 @@
  */
 package io.opentracing.mock;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import io.opentracing.References;
 import io.opentracing.Span;
