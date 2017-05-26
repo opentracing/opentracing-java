f3d31a5770317c5c226d09d03b39859ecf54d887
Brian Hanafee <bhanafee@gmail.com>
2017-05-25 21:39:26 -0700

    Remove unnecessary imports

--- opentracing-mock/src/main/java/io/opentracing/mock/MockTracer.java
+++ opentracing-mock/src/main/java/io/opentracing/mock/MockTracer.java
@@ -25,9 +25,7 @@ import io.opentracing.propagation.Format;
 import io.opentracing.propagation.TextMap;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
