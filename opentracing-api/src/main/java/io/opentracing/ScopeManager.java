037dffee5fa39bc533e9998510bc90df1f9ee82e
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2017-09-25 20:11:37 +0900

    Rename ScopeManager's Span activation parameter to finishSpanOnClose.
    The previous name was finishOnClose, which is not clear on *what*
    it closes.

--- opentracing-api/src/main/java/io/opentracing/ScopeManager.java
+++ opentracing-api/src/main/java/io/opentracing/ScopeManager.java
@@ -30,7 +30,7 @@ public interface ScopeManager {
      * programming error to neglect to call {@link Scope#close()} on the returned instance.
      */
     Scope activate(Span span);
-    Scope activate(Span span, boolean finishOnClose);
+    Scope activate(Span span, boolean finishSpanOnClose);
 
     /**
      * Return the currently active {@link Scope} which can be used to access the currently active
