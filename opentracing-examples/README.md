0ab157cd1291119610335cbe57f88aa0e1d198e4
Tyler Benson <tylerbenson@gmail.com>
2017-11-09 13:56:23 -0500

    Add Examples for async use cases (#197)
    * Add examples for async as test cases
    
    This includes execution flow similar to:
    * Actor ask/tell
    * Promises with callbacks
    * Work interleaved on a thread using suspend/resume.
    
    The implementations of these execution models are obviously very simplistic, but intended to emphasize the tracing aspect.

--- opentracing-examples/README.md
+++ opentracing-examples/README.md
@@ -10,10 +10,12 @@ List of patterns:
 - **activate_deactivate** - actions are executed by scheduler.   
 It shows continuation as a solution to finish span when last action is completed.
 - **active_span_replacement** - start an isolated task and query for its result in another task/thread
+- **actor_propagation** - tracing for blocking and non-blocking actor based tracing
 - **client_server** - typical client-server example
 - **common_request_handler** - one request handler for all requests
 - **late_span_finish** - late parent span finish
 - **listener_per_request** - one listener per request
 - **multiple_callbacks** - many callbacks spawned at the same time
 - **nested_callbacks** - one callback at the time, defined in a pipeline fashion
-
+- **promise_propagation** - tracing patterns for promises with callbacks
+- **suspend_resume_propagation** - tracing pattern for interleaving of spans
--- opentracing-examples/src/test/java/io/opentracing/examples/TestUtils.java
+++ opentracing-examples/src/test/java/io/opentracing/examples/TestUtils.java
@@ -17,6 +17,7 @@ import io.opentracing.mock.MockSpan;
 import io.opentracing.mock.MockTracer;
 import io.opentracing.tag.AbstractTag;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -37,18 +38,27 @@ public class TestUtils {
         };
     }
 
-    public static MockSpan getOneByTag(List<MockSpan> spans, AbstractTag key, Object value) {
-        MockSpan found = null;
+    public static List<MockSpan> getByTag(List<MockSpan> spans, AbstractTag key, Object value) {
+        List<MockSpan> found = new ArrayList<>(spans.size());
         for (MockSpan span : spans) {
             if (span.tags().get(key.getKey()).equals(value)) {
-                if (found != null) {
-                    throw new IllegalArgumentException("there is more than one span with tag '"
-                            + key.getKey() + "' and value '" + value + "'");
-                }
-                found = span;
+                found.add(span);
             }
         }
         return found;
+    }
+
+    public static MockSpan getOneByTag(List<MockSpan> spans, AbstractTag key, Object value) {
+        List<MockSpan> found = getByTag(spans, key, value);
+        if (found.size() > 1) {
+            throw new IllegalArgumentException("there is more than one span with tag '"
+                    + key.getKey() + "' and value '" + value + "'");
+        }
+        if (found.isEmpty()) {
+            return null;
+        } else {
+            return found.get(0);
+        }
     }
 
     public static void sleep() {
