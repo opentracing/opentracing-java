d3bb5275dd1bce631f78cc8acafd8906549575a5
Pavol Loffay <ploffay@redhat.com>
2017-11-30 10:16:29 +0100

    SpanBuilder deprecate startManual

--- opentracing-api/src/main/java/io/opentracing/Tracer.java
+++ opentracing-api/src/main/java/io/opentracing/Tracer.java
@@ -226,6 +226,12 @@ public interface Tracer {
         Scope startActive(boolean finishSpanOnClose);
 
         /**
+         * @deprecated use {@link #startManual} or {@link #startActive} instead.
+         */
+        @Deprecated
+        Span startManual();
+
+        /**
          * Like {@link #startActive()}, but the returned {@link Span} has not been registered via the
          * {@link ScopeManager}.
          *
@@ -233,12 +239,6 @@ public interface Tracer {
          * @return the newly-started Span instance, which has *not* been automatically registered
          *         via the {@link ScopeManager}
          */
-        Span startManual();
-
-        /**
-         * @deprecated use {@link #startManual} or {@link #startActive} instead.
-         */
-        @Deprecated
         Span start();
     }
 }
