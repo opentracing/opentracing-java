1b5a2258acf3c669be68b82e6f0deb58a03a63af
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2017-12-06 12:40:04 -0600

    Change BINARY to be resizable and stream-oriented. (#223)
    * Change BINARY to be resizable and stream-oriented.
    
    * Abstract the Binary adapter and have an Adapters class to return it.
    
    * Remove the isInbound/isOutbound methods from BinaryAdapter.
    
    * Make Binary use the Channel paradigm for injection/extraction
    
    * Have binary methods in Adapters named after inject/extract.
    
    * Add a BINARY propagator for MockTracer.
    
    * Throw RuntimeException in case of errors during BINARY's injection.
    
    * Put braces around if-blocks for Adapters/BinaryAdapter.
    
    * Use verbose messages for null parameters in Adapters.

--- opentracing-mock/src/test/java/io/opentracing/mock/MockTracerTest.java
+++ opentracing-mock/src/test/java/io/opentracing/mock/MockTracerTest.java
@@ -17,6 +17,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +31,7 @@ import io.opentracing.Scope;
 import io.opentracing.Span;
 import io.opentracing.SpanContext;
 import io.opentracing.Tracer;
+import io.opentracing.propagation.Adapters;
 import io.opentracing.propagation.Format;
 import io.opentracing.propagation.TextMapExtractAdapter;
 import io.opentracing.propagation.TextMapInjectAdapter;
@@ -202,6 +205,39 @@ public class MockTracerTest {
         Assert.assertEquals(finishedSpans.get(0).context().spanId(), finishedSpans.get(1).parentId());
     }
 
+    @Test
+    public void testBinaryPropagator() {
+        MockTracer tracer = new MockTracer(MockTracer.Propagator.BINARY);
+        {
+            Span parentSpan = tracer.buildSpan("foo")
+                    .start();
+            parentSpan.setBaggageItem("foobag", "fooitem");
+            parentSpan.finish();
+
+            ByteArrayOutputStream injectStream = new ByteArrayOutputStream();
+            tracer.inject(parentSpan.context(), Format.Builtin.BINARY,
+                    Adapters.injectBinary(injectStream));
+
+            ByteArrayInputStream extractStream = new ByteArrayInputStream(injectStream.toByteArray());
+            SpanContext extract = tracer.extract(Format.Builtin.BINARY, Adapters.extractBinary(extractStream));
+
+            Span childSpan = tracer.buildSpan("bar")
+                    .asChildOf(extract)
+                    .start();
+            childSpan.setBaggageItem("barbag", "baritem");
+            childSpan.finish();
+        }
+        List<MockSpan> finishedSpans = tracer.finishedSpans();
+
+        Assert.assertEquals(2, finishedSpans.size());
+        Assert.assertEquals(finishedSpans.get(0).context().traceId(), finishedSpans.get(1).context().traceId());
+        Assert.assertEquals(finishedSpans.get(0).context().spanId(), finishedSpans.get(1).parentId());
+        Assert.assertEquals("fooitem", finishedSpans.get(0).getBaggageItem("foobag"));
+        Assert.assertNull(finishedSpans.get(0).getBaggageItem("barbag"));
+        Assert.assertEquals("fooitem", finishedSpans.get(1).getBaggageItem("foobag"));
+        Assert.assertEquals("baritem", finishedSpans.get(1).getBaggageItem("barbag"));
+    }
+  
     @Test
     public void testActiveSpan() {
         MockTracer mockTracer = new MockTracer();
