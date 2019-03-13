473a45a9eb94934bacc357c95e0b20d0430fa667
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2019-03-13 17:23:51 +0100

    Add a clarification on why we are deprecating this.

--- opentracing-util/src/main/java/io/opentracing/util/AutoFinishScopeManager.java
+++ opentracing-util/src/main/java/io/opentracing/util/AutoFinishScopeManager.java
@@ -20,6 +20,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @deprecated use {@link ThreadLocalScopeManager} instead.
+ * The operation mode of this class contrasts with the 0.32
+ * deprecation of auto finishing {@link Span}s upon {@link Scope#close()}.
+ *
  * A {@link ScopeManager} implementation that uses ref-counting to automatically finish {@link Span}s.
  *
  * @see AutoFinishScope
